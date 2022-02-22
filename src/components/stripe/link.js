c0e958fe9f6a50dab25a06e24b2856cdf0b84e95
WeiProgramming <wei.leung1995@gmail.com>
2022-02-21 21:26:16 -0800

    stripe plaid public and acess token server links created and returning proper tokens

--- src/components/stripe/link.js
+++ src/components/stripe/link.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import axios from "axios";
+import React, { useCallback, useState, useEffect } from 'react'
+import axios from 'axios'
 
 import {
   usePlaidLink,
@@ -7,44 +7,58 @@ import {
   PlaidLinkOnEvent,
   PlaidLinkOnExit,
   PlaidLinkOptions,
-} from 'react-plaid-link';
+} from 'react-plaid-link'
 
 const PlaidLink = () => {
-  const [publicToken, sePublictToken] = useState(null);
-
-  // get link_token from your server when component mounts
-  React.useEffect(() => {
-    const createLinkToken = async () => {
-      const response =axios.post('/auth/public_token', {})
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      const tokenObj = await response.data;
-      sePublictToken(tokenObj.link_token);
-    };
-    createLinkToken();
-  }, []);
-
-  const onSuccess = useCallback<PlaidLinkOnSuccess>((publicToken, metadata) => {
+  const [pToken, sePublictToken] = useState(null)
+  const onSuccess = useCallback((pToken, metadata) => {
     // send public_token to your server
     // https://plaid.com/docs/api/tokens/#token-exchange-flow
-    console.log(publicToken, metadata);
-  }, []);
-
-  const { open, ready } = usePlaidLink({
-    publicToken,
+    console.log('success! sending public token for access token ')
+    console.log(pToken, metadata)
+    ExchangePublicToken(pToken)
+  }, [])
+  const config = {
+    token: pToken,
     onSuccess,
-    // onEvent
-    // onExit
-  });
+  }
+
+  const { open, ready, error } = usePlaidLink(config)
+
+  // get link_token from your server when component mounts
+  useEffect(() => {
+    const createLinkToken = async () => {
+      axios
+        .post('/auth/public_token', {})
+        .then(function (response) {
+          console.log('received in client ', response)
+          const tokenObj = response.data
+          sePublictToken(tokenObj.link_token)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+    createLinkToken()
+  }, [])
+  const ExchangePublicToken = async (publicToken) => {
+    axios
+      .post('/auth/exchange_public_token', {
+        publicToken: publicToken,
+      })
+      .then(function (response) {
+        console.log('received access token in client ', response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
   return (
     <button onClick={() => open()} disabled={!ready}>
       Connect a bank account
     </button>
-  );
-};
+  )
+}
 
-export default PlaidLink;
+export default PlaidLink
